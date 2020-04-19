d717d2a8227bf67b5070ad8e98f4cedb1664c24a
Justin Lee <hello@leejustin.com>
2020-04-19 14:28:53 -0700

    Add exercise files for Lesson 3

---
+++ lesson-3-independent-releases/exercises/travis_env_var_exercise/server.js
@@ -0,0 +1,13 @@
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function main() {
+  while(true) {
+    console.log("Microservices rock!");
+    await sleep(5000);
+  }
+}
+
+main();
