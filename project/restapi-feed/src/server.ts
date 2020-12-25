874796fddfca58d255b07a33b0ca9c2e62da613b
Moemad-711 <mohammedemad4@gmail.com>
2020-12-25 19:58:45 +0200

    Update server.ts

--- project/restapi-feed/src/server.ts
+++ project/restapi-feed/src/server.ts
@@ -30,6 +30,8 @@ import {V0_FEED_MODELS, V0_USER_MODELS} from './controllers/v0/model.index';
 
   app.use('/api/v0/', IndexRouter);
 
+
+  
   // Root URI call
   app.get( '/', async ( req, res ) => {
     res.send( '/api/v0/' );
