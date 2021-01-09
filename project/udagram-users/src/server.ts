b76f2d7cd8d4064566a8ca890d4782147b30fed1
Eylül <eyluldicleyurdakul@gmail.com>
2021-01-09 17:45:11 +0100

    Split the api into 2 microservices

---
+++ project/udagram-users/src/server.ts
@@ -0,0 +1,44 @@
+import cors from 'cors';
+import express from 'express';
+import {sequelize} from './sequelize';
+
+import {IndexRouter} from './controllers/v0/index.router';
+
+import bodyParser from 'body-parser';
+import {config} from './config/config';
+import {V0_USER_MODELS} from './controllers/v0/model.index';
+
+
+(async () => {
+  await sequelize.addModels(V0_USER_MODELS);
+  await sequelize.sync();
+
+  const app = express();
+  const port = process.env.PORT || 8080;
+
+  app.use(bodyParser.json());
+
+  app.use(cors({
+    allowedHeaders: [
+      'Origin', 'X-Requested-With',
+      'Content-Type', 'Accept',
+      'X-Access-Token', 'Authorization',
+    ],
+    methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+    origin: config.url,
+  }));
+
+  app.use('/api/v0/', IndexRouter);
+
+  // Root URI call
+  app.get( '/', async ( req, res ) => {
+    res.send( '/api/v0/' );
+  } );
+
+
+  // Start the Server
+  app.listen( port, () => {
+    console.log( `server running ${config.url}` );
+    console.log( `press CTRL+C to stop server` );
+  } );
+})();
