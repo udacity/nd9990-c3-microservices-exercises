b3bedb135cc2bae308bb8252171772bfab4986e4
Mike Hallatt <mike.hallatt@gmail.com>
2021-03-27 16:31:20 +0000

    Echo environment variable to console

--- lesson-3-independent-releases/exercises/travis_env_var_exercise/server.js
+++ lesson-3-independent-releases/exercises/travis_env_var_exercise/server.js
@@ -6,6 +6,8 @@ function sleep(ms) {
 async function main() {
   while(true) {
     console.log("Microservices rock!");
+    const favoriteFood = process.env.FAVORITE_FOOD;
+    console.log(`My favorite food is ${favoriteFood}`);
     await sleep(5000);
   }
 }
