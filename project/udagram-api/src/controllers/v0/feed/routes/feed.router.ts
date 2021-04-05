5c8e3d4ba9ea02a64107f7230d9bf85b6ccd2f26
mhallatt <mike.hallatt@gmail.com>
2021-04-05 09:44:19 +0100

    Fix lint errors

--- project/udagram-api/src/controllers/v0/feed/routes/feed.router.ts
+++ project/udagram-api/src/controllers/v0/feed/routes/feed.router.ts
@@ -7,7 +7,7 @@ import * as c from '../../../../config/config';
 
 const router: Router = Router();
 
-export function requireAuth(req: Request, res: Response, next: NextFunction) {
+export function requireAuth(req: Request, res: Response, next: NextFunction): Response {
   if (!req.headers || !req.headers.authorization) {
     return res.status(401).send({message: 'No authorization headers.'});
   }
@@ -18,7 +18,7 @@ export function requireAuth(req: Request, res: Response, next: NextFunction) {
   }
 
   const token = tokenBearer[1];
-  return jwt.verify(token, c.config.jwt.secret, (err, decoded) => {
+  return jwt.verify(token, c.config.jwt.secret, (err) => {
     if (err) {
       return res.status(500).send({auth: false, message: 'Failed to authenticate.'});
     }
