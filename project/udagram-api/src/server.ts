9d881323acc74e0661e5a25ae2916211604ba40b
Justin Lee <hello@leejustin.com>
2021-07-30 01:02:04 -0700

    Add note about why CORS is set to Origin *

--- project/udagram-api/src/server.ts
+++ project/udagram-api/src/server.ts
@@ -21,6 +21,9 @@ import {V0_FEED_MODELS, V0_USER_MODELS} from './controllers/v0/model.index';
 
   app.use(bodyParser.json());
 
+  // We set the CORS origin to * so that we don't need to
+  // worry about the complexities of CORS this lesson. It's
+  // something that will be covered in the next course.
   app.use(cors({
     allowedHeaders: [
       'Origin', 'X-Requested-With',
