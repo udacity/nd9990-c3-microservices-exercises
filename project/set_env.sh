5f9706a39527d70f582518c6d621ca38a7325310
Antonio D'aria <antoniodaria@msn.com>
2021-10-10 12:40:35 +0100

    initial commit

--- project/set_env.sh
+++ project/set_env.sh
@@ -1,11 +1,10 @@
 # This file is used for convenience of local development.
-# DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=password1
-export POSTGRES_HOST=udagram2.csixzufepzrt.us-west-2.rds.amazonaws.com
-export POSTGRES_DB=udagram
-export AWS_BUCKET=arn:aws:s3:::udaconnect
-export AWS_REGION=us-west-2
-export AWS_PROFILE=test
-export JWT_SECRET=testing
+export POSTGRES_USERNAME=antonio
+export POSTGRES_PASSWORD=Aw3s0meyou
+export POSTGRES_HOST=ad-monolith-dev.clsnflhd2bh7.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=monolith-proj-180228063476-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=hello
 export URL=http://localhost:8100
