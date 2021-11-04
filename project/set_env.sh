d1686f7965474d6604251228235de4187c3b8771
Miracleiloh <ilohmiracle@yahoo.com>
2021-11-04 17:53:31 +0100

    CI with travis

--- project/set_env.sh
+++ project/set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=password1
-export POSTGRES_HOST=udagram2.csixzufepzrt.us-west-2.rds.amazonaws.com
-export POSTGRES_DB=udagram
-export AWS_BUCKET=arn:aws:s3:::udaconnect
-export AWS_REGION=us-west-2
-export AWS_PROFILE=test
+export POSTGRES_USERNAME=democity
+export POSTGRES_PASSWORD=democity
+export POSTGRES_HOST=democity.chgeyoijmfd1.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=democity
+export AWS_BUCKET=arn:aws:s3:::democity
+export AWS_REGION=us-east-1
+export AWS_PROFILE=democity
 export JWT_SECRET=testing
 export URL=http://localhost:8100
