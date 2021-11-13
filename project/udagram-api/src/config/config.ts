3421daaa1b158974b1a3e4d853ad4b74e513edfb
Elmir Rustemovic <elmir.rustemovic@arvato.com>
2021-11-13 10:19:33 +0100

    microservices

--- project/udagram-api/src/config/config.ts
+++ project/udagram-api/src/config/config.ts
@@ -1,12 +1,12 @@
 export const config = {
   'username': process.env.POSTGRES_USERNAME,
   'password': process.env.POSTGRES_PASSWORD,
-  'database': process.env.POSTGRES_DB,
+  'database': process.env.POSTGRES_DATABASE,
   'host': process.env.POSTGRES_HOST,
   'dialect': 'postgres',
   'aws_region': process.env.AWS_REGION,
   'aws_profile': process.env.AWS_PROFILE,
-  'aws_media_bucket': process.env.AWS_BUCKET,
+  'aws_media_bucket': process.env.AWS_MEDIA_BUCKET,
   'url': process.env.URL,
   'jwt': {
     'secret': process.env.JWT_SECRET,
