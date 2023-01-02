feb2cf3e9949f8e3ae32c29e2ae5d13c6236619a
HOANG NGUYEN <hoangthainguyen4121@gmail.com>
2023-01-02 18:25:00 +0700

    Update

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://abbd59b4a0f4f4ecc8757de09c4640dd-72751653.us-east-1.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://localhost:8080/api/v0'
 };
 
 /*
